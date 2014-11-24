8d8e81a505b3307a90644a8e7e8e7c31c45e9046
mark h <hopkinm@localhost.localdomain>
2014-11-24 07:51:37 -0800

    changed main

--- src/main.java
+++ src/main.java
@@ -3,6 +3,11 @@ public class main {
 
 	public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+	}
+	
+	public int addNumbers(int i, int j){
+		return (i + j);
 	}
 
 }
